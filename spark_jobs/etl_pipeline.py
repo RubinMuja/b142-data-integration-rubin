064246b0d6d40a5702455f87058acbb156281c07
RubinMuja <rubmuja10@gmail.com>
2025-06-28 15:59:07 +0200

    Update etl_pipeline.py

--- spark_jobs/etl_pipeline.py
+++ spark_jobs/etl_pipeline.py
@@ -1,24 +1,24 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, avg
 
-# Step 1: Start Spark
+#1: Start Spark
 spark = SparkSession.builder \
     .appName("COVID-19 Data Integration") \
     .getOrCreate()
 
-# Step 2: Load dataset
+#2: Load dataset
 df = spark.read.csv("data/epidemiology.csv", header=True, inferSchema=True)
 
-# Step 3: Clean data
+#3: Clean data
 df_clean = df.dropna(subset=["date", "location_key", "new_confirmed"])
 
-# Step 4: Filter for key countries
+#4: Filter for key countries
 filtered = df_clean.filter(col("location_key").isin("DE", "IT", "US"))
 
-# Step 5: Compute average daily confirmed cases per country
+#5: Compute average daily confirmed cases per country
 result = filtered.groupBy("location_key").agg(avg("new_confirmed").alias("avg_daily_cases"))
 
-# Step 6: Save results to Parquet
+#6: Save results to Parquet
 result.write.mode("overwrite").parquet("output/avg_daily_cases.parquet")
 
 spark.stop()
